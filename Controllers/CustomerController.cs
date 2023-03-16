2502f2e830dc3fd26a6b764f779d41a075c4a833
jgrissom <jgrissom91@gmail.com>
2023-03-16 14:04:47 -0500

    Customer Form

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class CustomerController : Controller
+{
+    private DataContext _dataContext;
+    public CustomerController(DataContext db) => _dataContext = db;
+
+    public ActionResult Register() => View();
+}
+
